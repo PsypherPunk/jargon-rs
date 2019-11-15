369dcc83c58bdabbcd237daa6adcd47feb6f7fc5
PsypherPunk <psypherpunk@gmail.com>
2019-11-15 09:53:02 +0000

    Update Project Structure
    * Split JSON parsing fuctionality into a separate `lib.rs`.
    * Use the above *lib* in a separate `main.rs`.

---
+++ src/bin/main.rs
@@ -0,0 +1,11 @@
+extern crate jargon;
+
+use colored::*;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+
+fn main() {
+    let choice: &jargon::Jargon = jargon::JARGON.choose(&mut thread_rng()).unwrap();
+    println!("{}\n\n{}", choice.term.bold(), choice.definition.italic());
+}
