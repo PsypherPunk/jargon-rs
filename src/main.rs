dfce415bca9ff970e79921ab94ab67c30c14f2d7
PsypherPunk <psypherpunk@gmail.com>
2020-08-03 20:14:31 +0100

    Add Argument Parsing to Display Version
    Add [`argh`](https://github.com/google/argh), currently including a
    `--version` flag.
    
    +semver: feature

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-extern crate jargon;
-
-use colored::*;
+use argh::FromArgs;
+use colored::Colorize;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+#[derive(FromArgs)]
+/// Show an entry from the Jargon File.
+struct Args {
+    #[argh(switch, short = 'v')]
+    /// display the current version
+    version: bool,
+}
+
 fn main() {
-    let choice: &jargon::Jargon = jargon::JARGON.choose(&mut thread_rng()).unwrap();
-    println!("{}\n\n{}", choice.term.bold(), choice.definition.italic());
+    let args: Args = argh::from_env();
+    if args.version {
+        println!("{}", VERSION);
+    } else {
+        let choice: &jargon::Jargon = jargon::JARGON.choose(&mut thread_rng()).unwrap();
+        println!("{}\n\n{}", choice.term.bold(), choice.definition.italic());
+    }
 }
